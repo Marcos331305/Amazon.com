26239a20b23b584136484c1dca885651a3d6623a
Marcos331305 <139604169+Marcos331305@users.noreply.github.com>
2024-08-25 18:37:31 +0530

    generate HTML for chekcout(cart) page & fix the delivery option selecting bug between different products

--- data/cart.js
+++ data/cart.js
@@ -1,5 +1,16 @@
 // creating the cartArray for a list of products
-export const cart = [];
+export const cart = [
+  {
+    id: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    productName:'Black and Gray Athletic Cotton Socks - 6 Pairs',
+    quantity: 1
+  },
+  {
+    id: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    productName:'Intermediate Size Basketball',
+    quantity: 2
+  }
+];
 
 /*
 function to add the product in the cart if it not in the cart. And if the product is already in the cart then it will only update the quantity.
@@ -20,6 +31,5 @@ export function addToCart(productId,productName) {
         quantity: 1,
       });
     }
-    console.log(cart);
   }
   
